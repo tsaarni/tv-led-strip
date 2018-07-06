b363e23642dc863f1588b0e8dbda6520e5947924
Tero Saarni <tero.saarni@gmail.com>
2018-07-06 12:19:50 +0300

    cleanup, updated themes and readme

--- README.md
+++ README.md
@@ -1,15 +1,47 @@
-# LED strip remote control for TV backlight
+# Remote control for TV background light
 
 ## Overview
 
+This is a small project implementing remote controllable background light for a TV.
+
+The software has a set of themes that can be set at compile time. A theme consist of four colors, one per each corner of the TV. The software calculates linear gradient between the corners.
+
+![Imgur](https://i.imgur.com/UBVnCq4l.jpg)
+
+
+[SK6812](https://cdn-shop.adafruit.com/product-files/1138/SK6812+LED+datasheet+.pdf) programmable LEDs are used as a light source. These chips consist of red, green, blue and white LEDs.
+
+![Imgur](https://i.imgur.com/1c0YrRfl.jpg?1)
+
+
+[TSOP34838](https://www.vishay.com/docs/82489/tsop322.pdf) IR receiver is used to remotely control the selection of color theme and to set the overall brightness of the theme. The settings are written to EEPROM on Atmega328 microcontroller to persist the selection even when the power is off.
+
+Here is a prototype of the project, connected to a small breadboard
+
+![Imgur](https://i.imgur.com/LPQrCSql.jpg)
+
+
+## Schematic
+
+[![tv-led-strip by tero.saarni@gmail.com 4fbfcfbe96263ceb - Upverter](https://upverter.com/tero.saarni@gmail.com/4fbfcfbe96263ceb/tv-led-strip/embed_img/15308997390000/)](https://upverter.com/tero.saarni@gmail.com/4fbfcfbe96263ceb/tv-led-strip/#/)
+
+
+## Dependencies
+
+[PlatformIO](https://platformio.org/) is used to build the project.
+
+Following libraries are used:
+
+* [light_ws2812](https://github.com/cpldcpu/light_ws2812) to control the LEDs
+* [IRLib2](https://github.com/cyborg5/IRLib2) to receive remote control codes
 
 
 ## Compiling
 
-To compile and upload:
+Execute following command to compile the program and upload it to the microcontroller:
 
     platformio run
 
-To see debug logs:
+Execute following command to follow the debug logs:
 
     platformio serialports monitor -b 115200
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 //
 // Remote control for LED-strip
 //
-// Copyright (C) 2018 tero.saarni@gmail.com
+// Copyright 2018 tero.saarni@gmail.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 #include <Arduino.h>
@@ -19,27 +31,34 @@ const uint8_t num_leds_vertical = 22;
 // total number of leds
 const uint8_t num_leds = (num_leds_horizontal*2) + (num_leds_vertical*2);
 
-// output buffer for writing led values out
+// output buffer for writing led values to led strip
 struct cRGBW leds[num_leds] = {0};
 
-
+// swap rgb -> grb
 #define COLOR(r,g,b,w) {g,r,b,w}
 
-// color themes for LEDS. Four colors per theme: top left, top right, bottom left, bottom right
+// color themes for leds.
+// four colors per theme, one for each corner of the tv,
+// order: top left, top right, bottom left, bottom right
 struct cRGBW themes[][4] = {
    {
       COLOR(0,0,0,255), COLOR(0,0,0,255),
-      COLOR(0,0,0,255), COLOR(0,0,0,255)
+      COLOR(0,0,0,255), COLOR(0,0,0,255),
    },
 
    {
       COLOR(0,0,0,255), COLOR(0,0,0,255),
+      COLOR(0,0,0,155), COLOR(0,0,0,155),
+   },
+
+   {
+      COLOR(0,0,0,155), COLOR(0,0,0,155),
       COLOR(0,0,0,255), COLOR(0,0,0,255),
    },
 
    {
       COLOR(100,120,170,0), COLOR(100,120,170,0),
-      COLOR(50,60,85,0),    COLOR(50,60,85,0),
+      COLOR(20,30,55,0),    COLOR(20,30,55,0),
    },
 };
 
@@ -47,14 +66,19 @@ struct cRGBW themes[][4] = {
 const size_t num_themes = sizeof(themes) / sizeof(themes[0]);
 
 
-// program status, also persisted in EEPROM
-int8_t current_theme;
-int8_t brightness;
+// program state, also persisted in EEPROM
+struct {
+   uint8_t theme_num;
+   uint8_t brightness;
+} state;
 
 
-// IR receiver
+// ir receiver (pin must support interrupt)
 IRrecvPCI ir_receiver(2);
 IRdecode ir_decoder;
+
+// ir protocol
+const uint8_t ir_protocol_num = SONY;
 
 // button codes
 const uint16_t button_theme_next      = 0x32E9;
@@ -70,21 +94,21 @@ const uint16_t button_brightness_down = 0x72E9;
 // calculate linear color gradient from color_a to color_b
 void fill_with_gradient(struct cRGBW color_a, struct cRGBW color_b, struct cRGBW* dest, uint16_t num_values)
 {
-   double r_steps = (double)(color_b.r - color_a.r) / num_values;
-   double g_steps = (double)(color_b.g - color_a.g) / num_values;
-   double b_steps = (double)(color_b.b - color_a.b) / num_values;
-   double w_steps = (double)(color_b.w - color_a.w) / num_values;
+   double r_step = (double)(color_b.r - color_a.r) / num_values;
+   double g_step = (double)(color_b.g - color_a.g) / num_values;
+   double b_step = (double)(color_b.b - color_a.b) / num_values;
+   double w_step = (double)(color_b.w - color_a.w) / num_values;
 
    for (uint16_t i=0; i<num_values; i++)
    {
-      uint8_t r = r_steps * i + color_a.r;
-      uint8_t g = g_steps * i + color_a.g;
-      uint8_t b = b_steps * i + color_a.b;
-      uint8_t w = w_steps * i + color_a.w;
-      dest[i].r = r * brightness / 100;
-      dest[i].g = g * brightness / 100;
-      dest[i].b = b * brightness / 100;
-      dest[i].w = w * brightness / 100;
+      uint8_t r = r_step * i + color_a.r;
+      uint8_t g = g_step * i + color_a.g;
+      uint8_t b = b_step * i + color_a.b;
+      uint8_t w = w_step * i + color_a.w;
+      dest[i].r = r * state.brightness / 100;
+      dest[i].g = g * state.brightness / 100;
+      dest[i].b = b * state.brightness / 100;
+      dest[i].w = w * state.brightness / 100;
    }
 }
 
@@ -92,7 +116,7 @@ void fill_with_gradient(struct cRGBW color_a, struct cRGBW color_b, struct cRGBW
 // write colors out to the leds
 void set_theme(struct cRGBW* theme)
 {
-   Serial.println("update leds");
+   Serial.println("updating leds");
    fill_with_gradient(theme[2], theme[0], &leds[0], num_leds_vertical);
    fill_with_gradient(theme[0], theme[1], &leds[num_leds_vertical], num_leds_horizontal);
    fill_with_gradient(theme[1], theme[3], &leds[num_leds_vertical+num_leds_horizontal], num_leds_vertical);
@@ -101,32 +125,38 @@ void set_theme(struct cRGBW* theme)
 }
 
 
-// persist program state to EEPROM
+// persist program state to eeprom
 void persist_program_state()
 {
-   EEPROM.update(0, current_theme);
-   EEPROM.update(1, brightness);
+   uint8_t *p = reinterpret_cast<uint8_t*>(&state);
+   for (size_t i=0; i<sizeof(state); i++)
+   {
+      EEPROM.update(i, *p++);
+   }
 }
 
-// restore program state from EEPROM
+// restore program state from eeprom
 void restore_program_state()
 {
-   current_theme = EEPROM.read(0);
-   brightness = EEPROM.read(1);
+   uint8_t *p = reinterpret_cast<uint8_t*>(&state);
+   for (size_t i=0; i<sizeof(state); i++)
+   {
+      *p++ = EEPROM.read(i);
+   }
 
-   // reset defaults when EEPROM is uninitialized
-   if (current_theme == 255) current_theme = 0;
-   if (brightness == 255)    brightness = 100;
+   // reset defaults when eeprom is uninitialized
+   if (state.theme_num  == 255) state.theme_num  = 0;
+   if (state.brightness == 255) state.brightness = 100;
    persist_program_state();
 
    Serial.print("number of leds: ");
    Serial.println(num_leds);
    Serial.print("number of themes: ");
    Serial.println(num_themes);
-   Serial.print("program state in eeprom: current_theme=");
-   Serial.print(current_theme);
+   Serial.print("program state in eeprom: theme_num=");
+   Serial.print(state.theme_num);
    Serial.print(", brightness=");
-   Serial.println(brightness);
+   Serial.println(state.brightness);
 }
 
 
@@ -136,9 +166,9 @@ void setup()
    restore_program_state();
 
    Serial.print("setting theme: ");
-   Serial.println(current_theme);
+   Serial.println(state.theme_num);
 
-   set_theme(themes[current_theme]);
+   set_theme(themes[state.theme_num]);
    ir_receiver.enableIRIn();
 }
 
@@ -151,49 +181,37 @@ void loop()
       ir_decoder.decode();
       //ir_decoder.dumpResults(true);
 
-      if (ir_decoder.protocolNum == SONY)
+      if (ir_decoder.protocolNum == ir_protocol_num)
       {
          bool state_changed = false;
 
          switch (ir_decoder.value)
          {
             case button_theme_next:
-               current_theme++;
-               if (current_theme == num_themes)
-                  current_theme = 0;
+               state.theme_num = (state.theme_num + 1) % num_themes;
                Serial.print("next theme: ");
-               Serial.println(current_theme);
+               Serial.println(state.theme_num);
                state_changed = true;
                break;
 
             case button_theme_prev:
-               current_theme--;
-               if (current_theme < 0)
-                  current_theme = num_themes - 1;
+               state.theme_num = (state.theme_num + num_themes - 1) % num_themes;
                Serial.print("previous theme: ");
-               Serial.println(current_theme);
+               Serial.println(state.theme_num);
                state_changed = true;
                break;
 
             case button_brightness_up:
+               state.brightness = (state.brightness + 10) > 100 ? 100 : state.brightness + 10;
                Serial.print("brightness up: ");
-
-               brightness = brightness + 10;
-               if (brightness > 100)
-                  brightness = 100;
-
-               Serial.println(brightness);
+               Serial.println(state.brightness);
                state_changed = true;
                break;
 
             case button_brightness_down:
+               state.brightness = (state.brightness - 10) < 10 ? 10 : state.brightness - 10;
                Serial.print("brightness down: ");
-
-               brightness = brightness - 10;
-               if (brightness < 10)
-                  brightness = 10;
-
-               Serial.println(brightness);
+               Serial.println(state.brightness);
                state_changed = true;
                break;
 
@@ -202,9 +220,9 @@ void loop()
          }
          if (state_changed)
          {
-            set_theme(themes[current_theme]);
+            set_theme(themes[state.theme_num]);
             persist_program_state();
-            delay(150);
+            delay(150);  // prevent too fast repeated button presses
          }
       }
    }
